dab6a1027cb9ca9c84fac13d19cbbde802076ad6
Alejandro Claveir <alejandro.claveir@gmail.com>
2016-08-26 11:44:01 -0300

    Add root route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'wellcome#index'
   mount Sidekiq::Web => '/sidekiq'
 end
