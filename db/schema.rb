8af5a15de8bcc33de21d35fa2e557f4292126aec
Alejandro Claveir <alejandro.claveir@gmail.com>
2016-08-27 15:48:26 -0300

    Se agrego funcionalidad para login en la app.

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20160827175926) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "name"
+    t.string   "sube_reference"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
