c09076cf5aa620ea69862501ee6ebd10df6f40b5
Alejandro Claveir <alejandro.claveir@gmail.com>
2016-08-27 15:07:40 -0300

    Configuracion inicial, devise, rolify, cancancan.

--- app/controllers/wellcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WellcomeController < ApplicationController
-
-  def index
-
-  end
-  
-end
--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WellcomeControllerTest < ActionDispatch::IntegrationTest
+class RoleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
