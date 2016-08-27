c90aff976907990efcc56b0d6172d3a7a770bed5
Alejandro Claveir <alejandro.claveir@gmail.com>
2016-08-27 17:00:07 -0300

    actualizacion de configuracion para entorno de produccion.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,10 +31,10 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Add the fonts path
-  #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Precompile additional assets
-  #config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
