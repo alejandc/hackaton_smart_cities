0edce51b9ba262721a86b870755ae52657c86aac
Alejandro Claveir <alejandro.claveir@gmail.com>
2016-08-27 18:20:38 -0300

    Se customizo registracion de usuario con devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:registrations => "registration"}
 
   get 'example', to: :index, controller: 'example'
 
