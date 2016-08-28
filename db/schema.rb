965bf77baa99029d7bb61e6c239409f60107a399
Martin Carniello <martincarniello@gmail.com>
2016-08-28 03:28:52 -0300

    Gems recollected...

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20160828042953) do
     t.string   "suggestion_type"
     t.string   "reference"
     t.boolean  "is_valid"
-    t.datetime "start_time"
+    t.datetime "start_start"
     t.datetime "end_time"
     t.integer  "points"
     t.datetime "created_at",      null: false
